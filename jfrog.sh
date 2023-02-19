b3ecf2d68585aa5fa0a4ff9cd51f716f87ff3189
ARehaman493 <97267532+ARehaman493@users.noreply.github.com>
2023-02-19 17:43:54 -0500

    Create jfrog.sh

---
+++ jfrog.sh
@@ -0,0 +1,20 @@
+java -version
+sudo apt update
+apt install git
+apt install maven
+sudo apt install openjdk-11-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins.service
+sudo systemctl status jenkins
+exit
+wget -O jfrog-deb-installer.tar.gz "https://releases.jfrog.io/artifactory/jfrog-prox/org/artifactory/pro/deb/jfrog-platform-trial-prox/[RELEASE]/jfrog-platform-trial-prox-[RELEASE]-deb.tar.gz"
+tar -xvzf jfrog-deb-installer.tar.gz
+sudo apt-get update
+sudo apt-get install jq
+cd jfrog-platform-trial-pro*
+sudo ./install.sh
+sudo systemctl start artifactory.service
+sudo systemctl start xray.service
